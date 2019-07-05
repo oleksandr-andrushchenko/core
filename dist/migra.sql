3601ef38864f4d862bbd7fe1fbaf5e8b427b1c93
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-07-05 21:23:57 +0300

    +rbac table added +user table - role to role_id renamved

--- dist/migra.sql
+++ dist/migra.sql
@@ -27,3 +27,22 @@ update `page` set `uri` = '/', `uri_hash` = md5('/') where `key` = 'index';
 
 drop table page_regular;
 drop table page_custom;
+
+alter table `user` change role role_id tinyint(1) unsigned NOT NULL DEFAULT 0;
+
+CREATE TABLE `rbac` (
+  `rbac_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` tinyint(1) unsigned DEFAULT NULL,
+  `user_id` int(5) unsigned DEFAULT NULL,
+  `permission_id` tinyint(3) unsigned DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`rbac_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+update `user` set `role_id` = 2 where `role_id` = 1;
+update `user` set `role_id` = 1 where `role_id` = 3;
+
+truncate table rbac;
+insert into rbac(`role_id`, `permission_id`) values(1, 1), (2, 5), (2, 7), (2, 8), (2, 10), (2, 11), (3, 2), (3, 4), (4, 9), (4, 13);
