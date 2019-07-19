aaccf08b7318538d67396688044490fcd021eb72
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-07-19 18:28:49 +0300

    +underscore-to-camel-case method added +soft non-func re-factoring

--- src/Helper/Strings.php
+++ src/Helper/Strings.php
@@ -4,10 +4,21 @@ namespace SNOWGIRL_CORE\Helper;
 
 class Strings
 {
-    public static function replaceBracketsParams($string, array $params)
+    public static function replaceBracketsParams(string $input, array $params): string
     {
         return str_replace(array_map(function ($i) {
             return '{' . $i . '}';
-        }, array_keys($params)), $params, $string);
+        }, array_keys($params)), $params, $input);
+    }
+
+    public static function underscoreToCamelCase(string $input, $lcfirst = true): string
+    {
+        $output = implode('', array_map('ucfirst', explode('_', $input)));
+
+        if ($lcfirst) {
+            return lcfirst($output);
+        }
+
+        return $output;
     }
 }
