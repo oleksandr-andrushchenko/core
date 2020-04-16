67d4f96391efc20650ec9b8cb70c31d21283f727
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2020-04-16 19:58:16 +0300

    +dimensions support added +improvements +soft re-factoring

--- src/Images.php
+++ src/Images.php
@@ -2,9 +2,14 @@
 
 namespace SNOWGIRL_CORE;
 
+use GlobIterator;
+use Imagick;
+use SNOWGIRL_CORE\Command\BatchCommand;
+use Throwable;
+
 class Images
 {
-    public const EXTENSION = 'jpg';
+    private const EXTENSION = 'jpg';
 
     public const FORMAT_NONE = 0;
     public const FORMAT_HEIGHT = 1;
@@ -20,7 +25,10 @@ class Images
         self::FORMAT_AUTO
     ];
 
-    protected $app;
+    /**
+     * @var AbstractApp
+     */
+    private $app;
 
     public function __construct(AbstractApp $app)
     {
@@ -32,80 +40,18 @@ class Images
         return new Image($file);
     }
 
-    public function isLocalHash(Image $image)
-    {
-        if (null === $image->getIsLocalHash()) {
-            //@todo improve (check extension...)
-            $image->setIsLocalHash(32 == strlen($image->getFile()));
-        }
-
-        return $image->getIsLocalHash();
-    }
-
-    public function isLocalNonHash(Image $image)
-    {
-        if ($tmp = parse_url($image->getFile())) {
-            if (!isset($tmp['host'])) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public function isLocal(Image $image)
-    {
-        return $this->isLocalHash($image) || $this->isLocalNonHash($image);
-    }
-
-    protected function getInfo(Image $image)
-    {
-        if (null === $image->getInfo()) {
-            if ($this->isLocalHash($image)) {
-                $file = $this->getServerNameByHash($image->getFile());
-            } elseif ($this->isLocalNonHash($image)) {
-                $file = str_replace($this->app->config('domains.static'), '', $image->getFile());
-                $file = $this->app->dirs['@public'] . $file;
-            } else {
-                $file = $image->getFile();
-            }
-
-            $info = getimagesize($file);
-            $image->setInfo(is_array($info) ? $info : []);
-        }
-
-        return $image->getInfo();
-    }
-
-    public function getWidth(Image $image)
-    {
-        return $this->getInfo($image)[0] ?? null;
-    }
-
-    public function getHeight(Image $image)
-    {
-        return $this->getInfo($image)[1] ?? null;
-    }
-
-    public function getMime(Image $image)
-    {
-        return $this->getInfo($image)['mime'] ?? null;
-    }
-
     protected static function normalizeParam($format = self::FORMAT_NONE, $param = 0)
     {
         if (in_array($format, [self::FORMAT_CAPTION, self::FORMAT_AUTO]) && is_array($param)) {
             $tmp = [];
 
-            if (isset($param['height'])) {
-                $tmp[] = $param['height'];
-            } elseif (isset($param[0])) {
+            // width
+            if (isset($param[0])) {
                 $tmp[] = $param[0];
             }
 
-            if (isset($param['width'])) {
-                $tmp[] = $param['width'];
-            } elseif (isset($param[1])) {
+            // height
+            if (isset($param[1])) {
                 $tmp[] = $param[1];
             }
 
@@ -115,32 +61,9 @@ class Images
         return $param;
     }
 
-    public function getHashServerPath($format = self::FORMAT_NONE, $param = 0)
-    {
-        return implode('/', [
-            $this->app->dirs['@public'],
-            'img',
-            $format,
-            $this->normalizeParam($format, $param)
-        ]);
-    }
-
-    public function getServerNameByHash($hash, $format = self::FORMAT_NONE, $param = 0)
-    {
-        return $this->getServerNameByName($hash . '.' . self::EXTENSION, $format, $param);
-    }
-
-    public function getServerNameByName($name, $format = self::FORMAT_NONE, $param = 0)
-    {
-        return implode('/', [
-            $this->getHashServerPath($format, $param),
-            $name
-        ]);
-    }
-
     public function getLink(Image $image, $format = self::FORMAT_NONE, $param = 0, $domain = false)
     {
-        if ($this->isLocalHash($image)) {
+        if ($image->isLocalHash()) {
             return $this->app->router->makeLink('image', [
                 'format' => $format,
                 'param' => $this->normalizeParam($format, $param),
@@ -148,13 +71,48 @@ class Images
             ], $domain);
         }
 
-        if ($this->isLocalNonHash($image)) {
+        if ($image->isLocalNonHash()) {
             return $this->app->router->makeLink('default', [
                 'action' => $image->getFile()
             ], $domain);
         }
 
         return $image->getFile();
+    }
+
+    /**
+     * @return bool
+     */
+    public function tests(): bool
+    {
+        $this->test('/home/snowgirl/Downloads/girlway-tests-images/719x396.png');
+        $this->test('/home/snowgirl/Downloads/girlway-tests-images/495x700.jpg');
+
+        return true;
+    }
+
+    /**
+     * @todo should be always synced with Image\GetAction method
+     * @param Image $image
+     * @param int $format
+     * @param int $param
+     * @return array
+     */
+    public function getDimensions(Image $image, $format = self::FORMAT_NONE, $param = 0): array
+    {
+        if (Images::FORMAT_HEIGHT == $format) {
+            return $this->getHeightDimensions($image, $param);
+        } elseif (Images::FORMAT_WIDTH == $format) {
+            return $this->getWidthDimensions($image, $param);
+        } elseif (Images::FORMAT_CAPTION == $format) {
+            $param = explode('-', $param);
+            return [$param[0], $param[1 == count($param) ? 0 : 1]];
+        } elseif (Images::FORMAT_AUTO == $format) {
+            $param = explode('-', $param);
+            return [$param[0], $param[1 == count($param) ? 0 : 1]];
+        }
+
+        return [$image->getWidth(), $image->getHeight()];
     }
 
     public function getLinkByFile($file, $format = self::FORMAT_NONE, $param = 0, $domain = false)
@@ -165,28 +123,17 @@ class Images
     public function delete(Image $image, &$error = null)
     {
         try {
-            if ($this->isLocalHash($image)) {
-                $file = $image->getFile() . '.' . self::EXTENSION;
-
-                foreach (self::$formats as $format) {
-                    foreach (glob(implode('/', [
-                        $this->app->dirs['@public'],
-                        'img',
-                        $format,
-                        '*'
-                    ])) as $dir) {
-                        if (is_file($f = implode('/', [$dir, $file]))) {
-                            unlink($f);
-                        }
-                    }
+            if ($image->isLocalHash()) {
+                foreach (glob($this->getPathName('*', '*', $image->getFile())) as $file) {
+                    unlink($file);
                 }
-            } elseif ($this->isLocalNonHash($image)) {
+            } elseif ($image->isLocalNonHash()) {
                 false || false;
             }
 
             return true;
-        } catch (\Exception $ex) {
-            $error = $ex->getMessage();
+        } catch (Throwable $e) {
+            $error = $e->getMessage();
             return false;
         }
     }
@@ -200,7 +147,7 @@ class Images
     {
         $aff = 0;
 
-        foreach (glob($this->app->dirs['@public'] . '/img/*/*/' . $image . '.' . self::EXTENSION) as $file) {
+        foreach (glob($this->getPathName('*', '*', $image)) as $file) {
             if (unlink($file)) {
                 $aff++;
             }
@@ -211,23 +158,37 @@ class Images
 
     public function getLocalByFile($file): string
     {
-//        return substr(basename($file), 0, 32);
         return str_replace('.' . self::EXTENSION, '', basename($file));
     }
 
-    public function getAllLocalFiles(): array
+    public function getPathName($format, $param, $hash): string
     {
-        return glob($this->app->dirs['@public'] . '/img/0/0/*.' . self::EXTENSION);
+        return $this->app->dirs['@public'] . '/img/' . $format . '/' . $this->normalizeParam($format, $param) . '/' . $hash . '.' . self::EXTENSION;
+    }
+
+    public function walkLocal($format, $param, string $hash, callable $job, int $size = 1000): int
+    {
+        $path = $this->getPathName($format, $param, $hash);
+
+        if ('*' === $hash) {
+            $iterator = new GlobIterator($path, GlobIterator::CURRENT_AS_PATHNAME);
+        } else {
+            $iterator = glob($path);
+        }
+
+        $command = new BatchCommand($iterator, $job, $size);
+
+        return $command();
     }
 
     protected function optimizeImagick($imagick, $quality = 70)
     {
-        /** @var \Imagick $imagick */
+        /** @var Imagick $imagick */
         $imagick->stripImage();
-        $imagick->setImageCompression(\Imagick::COMPRESSION_JPEG);
+        $imagick->setImageCompression(Imagick::COMPRESSION_JPEG);
         $imagick->setImageCompressionQuality($quality);
-        $imagick->setImageInterlaceScheme(\Imagick::INTERLACE_PLANE);
-        $imagick->setInterlaceScheme(\Imagick::INTERLACE_PLANE);
+        $imagick->setImageInterlaceScheme(Imagick::INTERLACE_PLANE);
+        $imagick->setInterlaceScheme(Imagick::INTERLACE_PLANE);
 //        $imagick->gaussianBlurImage(0.8, 10);
     }
 
@@ -237,10 +198,9 @@ class Images
     }
 
     /**
-     * @param            $file
+     * @param $file
      * @param int $quality
-     * @param bool|false $replace
-     *
+     * @param bool $replace
      * @return array|bool|string
      */
     public function optimize($file, $quality = 85, $replace = false)
@@ -256,7 +216,7 @@ class Images
         }
 
         try {
-            $imagick = new \Imagick($file);
+            $imagick = new Imagick($file);
             $this->optimizeImagick($imagick, $quality);
 
             if ($replace) {
@@ -269,14 +229,24 @@ class Images
             $imagick->destroy();
 
             return $v ? $output : false;
-        } catch (\ImagickException $ex) {
+        } catch (Throwable $e) {
             return false;
         }
     }
 
-    public function getHash($target)
+    public function getHash($target): string
     {
         return md5($target);
+    }
+
+    public function getHashLength(): int
+    {
+        return 32;
+    }
+
+    public function getExtension(): string
+    {
+        return self::EXTENSION;
     }
 
     public function getFileName($target, $hash = null)
@@ -286,30 +256,30 @@ class Images
     }
 
     /**
-     * @todo add HEIGHT x WIDTH to hash name
-     *
-     * @param      $target
-     * @param null $hash
-     * @param null $error
-     *
+     * @param string $target
+     * @param string|null $hash
+     * @param string|null $error
      * @return bool|string
      */
-    public function download($target, $hash = null, &$error = null)
+    public function download(string $target, string $hash = null, string &$error = null)
     {
         try {
             $hash = $hash ?: $this->getHash($target);
-            $pathName = $this->getServerNameByHash($hash);
 
-            $imagick = new \Imagick($target);
+            $imagick = new Imagick($target);
             $this->optimizeImagick($imagick);
 
+            $height = $imagick->getImageHeight();
+            $width = $imagick->getImageWidth();
+
+            $name = $this->addDimensions($hash, $width, $height);
+            $pathName = $this->getPathName(Images::FORMAT_NONE, 0, $name);
+
             if ('image/png' == $imagick->getImageMimeType()) {
-//            if (('image/png' == $imagick->getImageMimeType() && (\Imagick::COLORSPACE_RGB == $imagick->getColorspace()))) {
-                $width = $imagick->getImageWidth();
-                $height = $imagick->getImageHeight();
-                $white = new \Imagick();
+//            if (('image/png' == $imagick->getImageMimeType() && (Imagick::COLORSPACE_RGB == $imagick->getColorspace()))) {
+                $white = new Imagick();
                 $white->newImage($width, $height, 'white');
-                $white->compositeimage($imagick, \Imagick::COMPOSITE_OVER, 0, 0);
+                $white->compositeimage($imagick, Imagick::COMPOSITE_OVER, 0, 0);
                 $imagick = $white;
             }
 
@@ -319,7 +289,7 @@ class Images
 //                $v = $hash;
 
                 if (0 < filesize($pathName)) {
-                    $v = $hash;
+                    $v = $name;
                 } else {
                     unlink($pathName);
                     $v = false;
@@ -330,10 +300,11 @@ class Images
 
             $imagick->destroy();
             return $v;
-        } catch (\Exception $ex) {
-            $error = $ex->getMessage();
-            return false;
+        } catch (Throwable $e) {
+            $error = $e->getMessage();
         }
+
+        return false;
     }
 
     public function downloadWithCurl($uri, $hash = null, &$error = null)
@@ -356,10 +327,11 @@ class Images
             $output = $this->download($local, $hash, $error);
             unlink($local);
             return $output;
-        } catch (\Exception $ex) {
-            $error = $ex->getMessage();
-            return false;
+        } catch (Throwable $e) {
+            $error = $e->getMessage();
         }
+
+        return false;
     }
 
     public function downloadWithWget($uri, $hash = null, &$error = null)
@@ -383,10 +355,11 @@ class Images
             $output = $this->download($local, $hash, $error);
             unlink($local);
             return $output;
-        } catch (\Exception $ex) {
-            $error = $ex->getMessage();
-            return false;
+        } catch (Throwable $e) {
+            $error = $e->getMessage();
         }
+
+        return false;
     }
 
     public function downloadRemote($uri, $hash = null, &$error = null)
@@ -414,5 +387,85 @@ class Images
 
         //@todo tmp_name always different for same files.. (its means no cache..)
         return $this->download($file['tmp_name'], null, $error);
+    }
+
+    /**
+     * @param string $hash
+     * @param int|null $width
+     * @param int|null $height
+     * @return string
+     * @throws Exception
+     */
+    public function addDimensions(string $hash, int $width = null, int $height = null): string
+    {
+        if (null === $width || null === $height) {
+            if (!$info = getimagesize($this->getPathName(self::FORMAT_NONE, 0, $hash))) {
+                throw new Exception('invalid getimagesize result');
+            }
+
+            $width = $info[0];
+            $height = $info[1];
+        }
+
+        return str_replace(['{hash}', '{width}', '{height}'], [$hash, $width, $height], '{hash}_{width}x{height}');
+    }
+
+    private function getHeightDimensions(Image $image, int $targetHeight): array
+    {
+        $height = $image->getHeight();
+        $width = $image->getWidth();
+        $ratio = $height / $width;
+        $newHeight = $targetHeight;
+        $newWidth = round($newHeight / $ratio);
+        return [$newWidth, $newHeight];
+    }
+
+    private function getWidthDimensions(Image $image, int $targetWidth): array
+    {
+        $height = $image->getHeight();
+        $width = $image->getWidth();
+        $ratio = $height / $width;
+        $newWidth = $targetWidth;
+        $newHeight = round($newWidth * $ratio);
+        return [$newWidth, $newHeight];
+    }
+
+    /**
+     * @param $file
+     * @throws \ImagickException
+     */
+    private function test($file)
+    {
+        $sizes = [
+            [300, 300],
+            [500, 500],
+            [800, 800],
+        ];
+
+        foreach (self::$formats as $format) {
+            foreach ($sizes as $size) {
+                $imagick = new Imagick($file);
+
+                if (Images::FORMAT_HEIGHT == $format) {
+                    $imagick->scaleImage(0, $size[1]);
+                    $imagick->writeImage(str_replace('.', '_height_' . implode('x', $size) . '.', $file));
+                } elseif (Images::FORMAT_WIDTH == $format) {
+                    $imagick->scaleImage($size[0], 0);
+                    $imagick->writeImage(str_replace('.', '_width_' . implode('x', $size) . '.', $file));
+                } elseif (Images::FORMAT_CAPTION == $format) {
+                    $imagick->scaleImage($size[0], $size[0]);
+                    $imagick->writeImage(str_replace('.', '_caption_1_' . implode('x', [$size[0], $size[0]]) . '.', $file));
+
+                    $imagick->scaleImage($size[0], $size[1]);
+                    $imagick->writeImage(str_replace('.', '_caption_2_' . implode('x', $size) . '.', $file));
+                } elseif (Images::FORMAT_AUTO == $format) {
+                    $imagick->cropThumbnailImage($size[0], $size[0]);
+                    $imagick->writeImage(str_replace('.', '_auto_1_' . implode('x', [$size[0], $size[0]]) . '.', $file));
+
+                    $imagick->cropThumbnailImage($size[0], $size[1]);
+                    $imagick->writeImage(str_replace('.', '_auto_2_' . implode('x', $size) . '.', $file));
+                }
+            }
+        }
     }
 }
