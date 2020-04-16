04307b2746ad8d59eaa1865611cbe7cb3808058f
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2020-04-16 20:03:26 +0300

    +index-one and get-alias-indexes methods removed

--- src/Indexer/IndexerManagerInterface.php
+++ src/Indexer/IndexerManagerInterface.php
@@ -8,11 +8,7 @@ interface IndexerManagerInterface
 
     public function deleteIndex(string $name): bool;
 
-    public function indexOne(string $index, $id, array $document): bool;
-
     public function indexMany(string $index, array $documents): int;
-
-    public function getAliasIndexes(string $alias, $withAliasOnly = false): array;
 
     public function switchAliasIndex(string $alias, string $index = null, callable $work = null);
 }
