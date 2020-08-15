dc8595caaddf5e60c459d37696e75bfe1ca18ab5
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2020-08-15 17:26:40 +0300

    +validate method added & implemented

--- src/Controller/Outer/SubscribeAction.php
+++ src/Controller/Outer/SubscribeAction.php
@@ -5,6 +5,7 @@ namespace SNOWGIRL_CORE\Controller\Outer;
 use SNOWGIRL_CORE\Http\HttpApp as App;
 use SNOWGIRL_CORE\Http\Exception\MethodNotAllowedHttpException;
 use SNOWGIRL_CORE\View\Layout;
+use SNOWGIRL_CORE\View\Widget\Form\Subscribe;
 
 class SubscribeAction
 {
@@ -20,15 +21,35 @@ class SubscribeAction
         if ($app->request->isGet()) {
             if ($app->request->get('code')) {
                 $isOk = $form->confirm($app->request, $msg);
-                $output = ['isOk' => $isOk, 'body' => $msg];
+
+                $output = [
+                    'isOk' => $isOk,
+                    'body' => $msg,
+                ];
+
                 $view->addMessage($msg, $isOk ? Layout::MESSAGE_SUCCESS : Layout::MESSAGE_ERROR);
+
                 $app->request->redirectToRoute('default');
             } else {
-                $output = ['isOk' => true, 'body' => $form->stringify()];
+                $output = [
+                    'isOk' => true,
+                    'body' => $form->stringify(),
+                ];
             }
         } elseif ($app->request->isPost()) {
-            $isOk = $form->process($app->request, $msg);
-            $output = ['isOk' => $isOk, 'body' => $msg];
+            if ($this->validate($app, $form)) {
+                $isOk = $form->process($app->request, $msg);
+
+                $output = [
+                    'isOk' => $isOk,
+                    'body' => $msg,
+                ];
+            } else {
+                $output = [
+                    'isOk' => false,
+                    'body' => '',
+                ];
+            }
         } else {
             throw (new MethodNotAllowedHttpException)->setValidMethod(['get', 'post']);
         }
@@ -40,5 +61,21 @@ class SubscribeAction
         } else {
             $app->response->setHTML(200, $view->setContent($output['body']));
         }
+    }
+
+    private function validate(App $app, Subscribe $subscribeForm)
+    {
+        $name = $subscribeForm->getParam('name');
+
+        if ($name && preg_match('/^[a-zA-Z]{10}$/', $name)) {
+            $app->container->logger->info('subscribe validation failed', [
+                'name' => $name,
+                'email' => $subscribeForm->getParam('email'),
+            ]);
+
+            return false;
+        }
+
+        return true;
     }
 }
